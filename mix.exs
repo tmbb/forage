06c0d8c85f5fabe18653898d3305548d54138a55
tmbb <tmbb@campus.ul.pt>
2018-12-01 15:34:29 +0000

    ok

--- mix.exs
+++ mix.exs
@@ -1,12 +1,16 @@
 defmodule Forage.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :forage,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,7 +27,21 @@ defmodule Forage.MixProject do
     [
       {:ecto, "~> 3.0"},
       {:phoenix_html, "~> 2.10"},
-      {:paginator, "~> 0.4.0"}
+      {:paginator, "~> 0.6.0"},
+      {:ex_doc, "~> 0.19", only: :dev}
+    ]
+  end
+
+  defp description() do
+    "Dynamic ecto query builder"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "forage",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tmbb/forage"}
     ]
   end
 end
