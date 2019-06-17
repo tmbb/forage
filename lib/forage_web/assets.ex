6665f88c4c6c1c05f09e6e61cdf59d06be24aeb8
tmbb <tmbb@campus.ul.pt>
2019-06-17 22:05:45 +0100

    fix assets

--- lib/forage_web/assets.ex
+++ lib/forage_web/assets.ex
@@ -3,8 +3,8 @@ defmodule ForageWeb.Assets do
   @activate_datepicker "<script>" <>
                          File.read!("lib/forage_web/assets/datepicker.js") <> "</script>"
   @forage_date_input_assets """
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet"/>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" rel="stylesheet"/>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
   """
   @forage_select_assets """
   <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
